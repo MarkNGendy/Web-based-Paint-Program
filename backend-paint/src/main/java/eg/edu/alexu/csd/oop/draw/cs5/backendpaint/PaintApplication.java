ac9dfd4db5788aed6a58eaeea9fbf8e97ab88d4f
MarkNGendy <markngendy@gmail.com>
2020-11-21 14:34:01 +0200

    Create backend project structure

---
+++ backend-paint/src/main/java/eg/edu/alexu/csd/oop/draw/cs5/backendpaint/PaintApplication.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.csd.oop.draw.cs5.backendpaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintApplication.class, args);
+	}
+
+}
