7cff353eb49e9963622b5c7bda76f5191eebf616
MarkNGendy <markngendy@gmail.com>
2020-11-28 15:10:46 +0200

    begin to code javascript

--- frontend-paint/src/components/Paint.vue
+++ frontend-paint/src/components/Paint.vue
@@ -1,198 +1,244 @@
 <template>
-  <div class="Paint">
-  <h1>{{msg}}</h1>
-  </div>
-  <div class="main">
-    <div>
-      <button class="opt">undo</button>
-      <button class="opt">redo</button>
-      <button class="opt">save</button>
-      <button class="opt">load</button>
-      <button class="opt">clear</button>
+    <div class="Paint">
+        <h1>{{ msg }}</h1>
     </div>
-    <div class= "shapes">
-      <button class="square"></button>
-      <button class="rectangle"></button>
-      <button class="circle"></button>
-      <button class="triangle"></button>
-      <button class="ellipse"></button>
-      <button class="line"></button>
+    <div class="main">
+        <div>
+            <button class="opt">Undo</button>
+            <button class="opt">Redo</button>
+            <button class="opt">Save</button>
+            <button class="opt">Load</button>
+            <button class="opt">Clear</button>
+        </div>
+        <div class="shapes">
+            <button class="square"></button>
+            <button class="rectangle"></button>
+            <button class="circle"></button>
+            <button class="triangle"></button>
+            <button class="ellipse"></button>
+            <button class="line"></button>
+        </div>
+        <div>
+            <button class="move"></button>
+            <button class="delete"></button>
+            <button class="resize"></button>
+        </div>
+        <label class="label">select the color:</label>
+        <input type="color" />
+        <span>{{ X }}, {{ y }}</span>
+        <canvas
+            class="drawing-board"
+        ></canvas>
     </div>
-    <div>
-      <button class="move"></button>
-      <button class="delete"></button>
-      <button class="resize"></button>
-    </div>
-    <label class="label">select the color:</label>
-    <input type="color"> 
-    <canvas class="drawing-board"></canvas>
-  </div>
 </template>
 
 <script>
-
+// import axios from "axios";
 export default {
-  name: "Paint",
-  props: {
-    msg: String
-  }
+    name: "Paint",
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            points: null,
+            canvas: null,
+            x: 0,
+            y: 0
+        };
+    },
+    methods: {
+        showCoordinates(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        },
+        printX() {
+            console.log(this.x);
+        },
+        drawLine(x1, x2, y1, y2) {
+            let ctx = this.canvas;
+            ctx.beginPath();
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            ctx.closePath();
+        },
+        beginDrawing(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+            this.isDrawing = true;
+        },
+        keepDrawing(e) {
+            if (this.isDrawing === true) {
+              this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+            }
+        },
+        stopDrawing(e) {
+            if (this.isDrawing) {
+                this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+                this.x = 0;
+                this.y = 0;
+                this.isDrawing = false;
+            }
+        }
+    }
 };
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.main{
-background-image: url("./cool-background.png");
-background-size: cover;
+.main {
+    background-image: url("./cool-background.png");
+    background-size: cover;
 }
-.shapes{
-  text-align: center;
+.shapes {
+    text-align: center;
 }
 .square {
-  height: 40px;
-  width: 40px;
-  background-image: url("./square.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./square.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.square:hover{
-  background-image: url("./sq\ hov.png");
-  }
+.square:hover {
+    background-image: url("./sq\ hov.png");
+}
 .rectangle {
-  height: 40px;
-  width: 40px;
-  background-image: url("./rectangle.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./rectangle.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.rectangle:hover{
-  background-image: url("./rec\ hov.png");
-  }
+.rectangle:hover {
+    background-image: url("./rec\ hov.png");
+}
 .ellipse {
-  height: 40px;
-  width: 40px;
-  background-image: url("./ellipse.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./ellipse.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.ellipse:hover{
-  background-image: url("./ell\ hov.png");
-  }
-.triangle{
-	height: 40px;
-  width: 40px;
-  background-image: url("./triangle.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+.ellipse:hover {
+    background-image: url("./ell\ hov.png");
 }
-.triangle:hover{
-  background-image: url("./tri\ hov.png");
-  }
+.triangle {
+    height: 40px;
+    width: 40px;
+    background-image: url("./triangle.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
+}
+.triangle:hover {
+    background-image: url("./tri\ hov.png");
+}
 .circle {
-  height: 40px;
-  width: 40px;
-  background-image: url("./circle.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
-  
+    height: 40px;
+    width: 40px;
+    background-image: url("./circle.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.circle:hover{
-  background-image: url("./circle\ ho.png");
-  }
+.circle:hover {
+    background-image: url("./circle\ ho.png");
+}
 .line {
-  height: 40px;
-  width: 40px;
-  background-image: url("./line.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./line.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.line:hover{
-  background-image: url("./line\ hov.png");
-  }
+.line:hover {
+    background-image: url("./line\ hov.png");
+}
 .move {
-  height: 40px;
-  width: 40px;
-  background-image: url("./move.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./move.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.move:hover{
-  background-image: url("./move\ hove.png");
+.move:hover {
+    background-image: url("./move\ hove.png");
 }
 .delete {
-  height: 40px;
-  width: 40px;
-  background-image: url("./del.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./del.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.delete:hover{
-  background-image: url("./del\ hov.png");
+.delete:hover {
+    background-image: url("./del\ hov.png");
 }
 .resize {
-  height: 40px;
-  width: 40px;
-  background-image: url("./resize.png");
-  background-size: cover;
-  border: none;
-  margin: 2px;
-  cursor: pointer;
+    height: 40px;
+    width: 40px;
+    background-image: url("./resize.png");
+    background-size: cover;
+    border: none;
+    margin: 2px;
+    cursor: pointer;
 }
-.resize:hover{
-  background-image: url("./resize\ hov.png");
+.resize:hover {
+    background-image: url("./resize\ hov.png");
 }
-.opt{
-  display:inline-block;
-  padding:0.35em 1.2em;
-  border:0.1em solid #000000;
-  margin:0 0.3em 0.3em 0;
-  border-radius: 0.25cm;
-  box-sizing: border-box;
-  text-decoration:none;
-  font-family:'Roboto',sans-serif;
-  font-weight:300;
-  color:#000000;
-  text-align:center;
-  cursor: pointer;
-  }
-.opt:hover{
-  color:#ffffff;
-  background-color:#000000;
-  }
-@media all and (max-width:30em){
-  .opt{
-  display:block;
-  margin:0.4em auto;
-  }
+.opt {
+    display: inline-block;
+    padding: 0.35em 1.2em;
+    border: 0.1em solid #000000;
+    margin: 0 0.3em 0.3em 0;
+    border-radius: 0.25cm;
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    color: #000000;
+    text-align: center;
+    cursor: pointer;
 }
-label{
-  color: #ffffff;
+.opt:hover {
+    color: #ffffff;
+    background-color: #000000;
 }
-.drawing-board{
-  cursor: crosshair;
-  background-color: rgb(255, 255, 255);
-  height: 80%;
-  width: 80%;
-  position: relative;
-  display: block; 
-  margin:auto;
-  outline: rgb(82, 82, 82) 3px solid;
+@media all and (max-width: 30em) {
+    .opt {
+        display: block;
+        margin: 0.4em auto;
+    }
+}
+label {
+    color: #ffffff;
+}
+.drawing-board {
+    cursor: crosshair;
+    background-color: rgb(255, 255, 255);
+    height: 80%;
+    width: 80%;
+    position: relative;
+    display: block;
+    margin: auto;
+    outline: rgb(82, 82, 82) 3px solid;
 }
 </style>
